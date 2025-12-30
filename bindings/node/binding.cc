afc2dea31f58159b5b5ee160217fd67ffe148388
Stanislaw Gruszka <stf_xl@wp.pl>
2025-12-30 11:15:14 +0100

    test: fix node binding test
    - update tree sitter
    - export name
    - add scanner.c unconditionally (just for sure)

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -10,6 +10,7 @@ const napi_type_tag LANGUAGE_TYPE_TAG = {
 };
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
+    exports["name"] = Napi::String::New(env, "bpftrace");
     auto language = Napi::External<TSLanguage>::New(env, tree_sitter_bpftrace());
     language.TypeTag(&LANGUAGE_TYPE_TAG);
     exports["language"] = language;
